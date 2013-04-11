e8f263fc000f09070284e3f91e44aebeac8a4096
parrot-studio <parrot.studio.dev@gmail.com>
2013-04-11 17:37:22 +0900

    footerを修正

--- theme-webcafe2.rb
+++ theme-webcafe2.rb
@@ -1,5 +1,5 @@
 include_theme("lightning-clear-blue")
 
 @slide_footer_info_left_text = canvas.title.gsub(/\n/, '')
-@slide_footer_info_right_text = 'by @parrot_studio #webcommcafe 2013/04/14'
+@slide_footer_info_right_text = 'by @parrot_studio for #webcommcafe 2013/04/14'
 include_theme("slide-footer-info")
